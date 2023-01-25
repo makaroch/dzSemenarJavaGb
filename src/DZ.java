9375657b2a1fd9762dc4734c02b05b142ce5d215
Alex <da.ek.ned@yandex.ru>
2023-01-25 11:35:50 +0300

    форматирование

--- src/DZ.java
+++ src/DZ.java
@@ -5,64 +5,62 @@ import java.util.Scanner;
 
 public class DZ {
     public static void main(String[] args) {
-        //System.out.println(task1());
-        //System.out.println(task2());
-        //System.out.println(2%);
-        //task3(1000);
+        // System.out.println(task1());
+        // System.out.println(task2());
+        // System.out.println(2%);
+        // task3(1000);
 
-        /*начало 4 задачки*/
-//        {
-//            double x = getNumberDouble("Введите число");
-//            double y = getNumberDouble("Введите число");
-//            String symbol = getChar("введите действие");
-//            System.out.println(task4(x, y, symbol));
-//        }
-        /*конец 4 задачки*/
+        /* начало 4 задачки */
+        // {
+        // double x = getNumberDouble("Введите число");
+        // double y = getNumberDouble("Введите число");
+        // String symbol = getChar("введите действие");
+        // System.out.println(task4(x, y, symbol));
+        // }
+        /* конец 4 задачки */
 
-        /*начало 5 задачки*/
+        /* начало 5 задачки */
         {// ?5 + ?1?? = 56
-        // ["?5?","+","?1??","=","5555"]
+         // ["?5?","+","?1??","=","5555"]
 
             String strPrimer = task5Viem();
             ArrayList<Integer> indexes = new ArrayList<Integer>();
 
-            for (int i = 0; i < strPrimer.length(); i++) {//добабим все элементы где есть вопросы в лист
-                if (strPrimer.charAt(i) == '?'){
+            for (int i = 0; i < strPrimer.length(); i++) {// добабим все элементы где есть вопросы в лист
+                if (strPrimer.charAt(i) == '?') {
                     indexes.add(i);
                 }
             }
-            if (indexes.size() == 0){
+            if (indexes.size() == 0) {
                 System.out.println("в веденом примере нет знака ?");
                 return;
             }
 
-  // ? = 4; 10000 001 002 9999
+            // ? = 4; 10000 001 002 9999
             int lengthFor = (int) Math.pow(10, indexes.size());
 
             String res = "";
-//            System.out.println(lengthFor);
+            // System.out.println(lengthFor);
 
             for (int i = 0; i < lengthFor; i++) {// i = 0
                 res = task5FindSolution(strPrimer, indexes, i);
-                if (res != ""){
+                if (res != "") {
                     System.out.println("решение: " + res);
                     return;
                 }
             }
             System.out.println("решений нет");
         }
-        /*конец 5 задачки*/
-
+        /* конец 5 задачки */
 
     }
-    
 
-    static String task5FindSolution(String primerStr, ArrayList list, int num){
+    static String task5FindSolution(String primerStr, ArrayList list, int num) {
         String numStr = Integer.toString(num);
-        while(numStr.length() < list.size()){
+        while (numStr.length() < list.size()) {
             numStr = "0" + numStr;
-        }// ? = 3 0 ; 021
-        
+        } // ? = 3 0 ; 021
+
         for (int i = 0; i < numStr.length(); i++) {
             StringBuilder sb = new StringBuilder(primerStr);
 
@@ -76,19 +74,18 @@ public class DZ {
         String[] arrStr = primerStr.split(" ");
         int[] arrInt = new int[3];
 
-        arrInt[0] = Integer.parseInt(arrStr[0]); //20
+        arrInt[0] = Integer.parseInt(arrStr[0]); // 20
         arrInt[1] = Integer.parseInt(arrStr[2]); // 30
-        arrInt[2] = Integer.parseInt(arrStr[4]); //56
+        arrInt[2] = Integer.parseInt(arrStr[4]); // 56
 
-        if (arrInt[2] == task4(arrInt[0], arrInt[1], arrStr[1])){
+        if (arrInt[2] == task4(arrInt[0], arrInt[1], arrStr[1])) {
             return primerStr;
         }
-        
+
         return "";
     }
 
-
-    static String task5Viem(){
+    static String task5Viem() {
         Scanner scan = new Scanner(System.in);
         System.out.println("введи пример");
         String s = scan.nextLine();
@@ -96,7 +93,7 @@ public class DZ {
         return s;
     }
 
-    static double task4(double num1, double num2, String symbol){
+    static double task4(double num1, double num2, String symbol) {
         return switch (symbol) {
             case "+" -> num1 + num2;
             case "-" -> num1 - num2;
@@ -105,12 +102,13 @@ public class DZ {
         };
 
     }
-    static void task3(int num){
-        //Вывести все простые числа от 1 до 1000
+
+    static void task3(int num) {
+        // Вывести все простые числа от 1 до 1000
         boolean flag = true;
         for (int i = 1; i < num; i++) {
             for (int j = 2; j < i; j++) {
-                if (i % j == 0){
+                if (i % j == 0) {
                     flag = false;
                     break;
                 }
@@ -121,8 +119,9 @@ public class DZ {
 
         }
     }
-    static int task2(){
-        //Вычислить n! (произведение чисел от 1 до n)
+
+    static int task2() {
+        // Вычислить n! (произведение чисел от 1 до n)
         int num = getNumberInt("Введите число");
         int mult = 1;
         for (int i = 1; i <= num; i++) {
@@ -131,17 +130,17 @@ public class DZ {
         return mult;
     }
 
-    static int task1(){
-        //(сумма чисел от 1 до n)
+    static int task1() {
+        // (сумма чисел от 1 до n)
         int num = getNumberInt("Введите число");
         int sum = 0;
         for (int i = 1; i <= num; i++) {
-            sum+= i;
+            sum += i;
         }
         return sum;
     }
 
-    static int getNumberInt(String message){
+    static int getNumberInt(String message) {
         System.out.println(message);
         Scanner scanner = new Scanner(System.in);
         if (scanner.hasNextInt())
@@ -150,7 +149,8 @@ public class DZ {
             return getNumberInt("Всетаки нужно ввести число");
 
     }
-    static double getNumberDouble(String message){
+
+    static double getNumberDouble(String message) {
         System.out.println(message);
         Scanner scanner = new Scanner(System.in);
         if (scanner.hasNextInt())
@@ -159,13 +159,15 @@ public class DZ {
             return getNumberDouble("Всетаки нужно ввести число");
 
     }
-    static String getChar(String message){
+
+    static String getChar(String message) {
         System.out.println(message);
         Scanner scanner = new Scanner(System.in);
         String symb = scanner.next();
-        if (symb.equals("+") || symb.equals("-") || symb.equals("*") || symb.equals("/") )
+        if (symb.equals("+") || symb.equals("-") || symb.equals("*") || symb.equals("/"))
             return symb;
-        else return getChar(message);
+        else
+            return getChar(message);
 
     }
 
